dcef7a07890e23ec47558fa96cc60d8e193de816
Ali Abdelfattah <ali.abdelfattah.xa@gmail.com>
2019-09-18 13:16:32 +0200

    Add NativeIdMatcher

--- detox/test/e2e/23.flows.test.js
+++
@@ -1,7 +0,0 @@
-describe('Flows', () => {
-
-  it('should exit without timeouts if app was terminated inside test', async () => {
-    await device.launchApp({newInstance: true});
-    await device.terminateApp();
-  });
-});
